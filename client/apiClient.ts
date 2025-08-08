2da62a6eb88ffa02f8acb39118ddbeb7bd117caa
Kade Wallis <kadewallis@gmail.com>
2025-08-08 12:10:05 +1200

    update weather.ts, apiClient with new API data

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -3,21 +3,21 @@ import { WeatherData } from '../models/weather'
 
 export async function getWeatherWellington() {
   const response = await request.get(
-    `https://api.open-meteo.com/v1/forecast?latitude=-41.2968672&longitude=174.7741143&current=temperature_2m`,
+    `https://api.open-meteo.com/v1/forecast?latitude=-41.2968672&longitude=174.7741143&current=temperature_2m,precipitation,wind_speed_10m,cloud_cover`,
   )
   return response.body as WeatherData
 }
 
 export async function getWeatherAuckland() {
   const response = await request.get(
-    `https://api.open-meteo.com/v1/forecast?latitude=-36.864486&longitude=174.776725&current=temperature_2m`,
+    `https://api.open-meteo.com/v1/forecast?latitude=-36.864486&longitude=174.776725&current=temperature_2m,precipitation,wind_speed_10m,cloud_cover`,
   )
   return response.body as WeatherData
 }
 
 export async function getWeatherChristchurch() {
   const response = await request.get(
-    `https://api.open-meteo.com/v1/forecast?latitude=-43.532055&longitude=172.63623&current=temperature_2m`,
+    `https://api.open-meteo.com/v1/forecast?latitude=-43.532055&longitude=172.63623&current=temperature_2m,precipitation,wind_speed_10m,cloud_cover`,
   )
   return response.body as WeatherData
 }
--- models/weather.ts
+++ models/weather.ts
@@ -1,23 +1,29 @@
 export interface WeatherData {
-  latitude: number
-  longitude: number
-  generationtime_ms: number
-  utc_offset_seconds: number
-  timezone: string
-  timezone_abbreviation: string
-  elevation: number
-  current_units: CurrentUnits
-  current: Current
+    latitude:              number;
+    longitude:             number;
+    generationtime_ms:     number;
+    utc_offset_seconds:    number;
+    timezone:              string;
+    timezone_abbreviation: string;
+    elevation:             number;
+    current_units:         CurrentUnits;
+    current:               Current;
 }
 
-export interface Current {
-  time: string
-  interval: number
-  temperature_2m: number
+interface Current {
+    time:           string;
+    interval:       number;
+    temperature_2m: number;
+    precipitation:  number;
+    wind_speed_10m: number;
+    cloud_cover:    number;
 }
 
-export interface CurrentUnits {
-  time: string
-  interval: string
-  temperature_2m: string
+interface CurrentUnits {
+    time:           string;
+    interval:       string;
+    temperature_2m: string;
+    precipitation:  string;
+    wind_speed_10m: string;
+    cloud_cover:    string;
 }
