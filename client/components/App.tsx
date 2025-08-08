dd2d1936fa86d689c26048ffd04a4f9c8f4c2a99
Annie Moffatt <a.e.moffatt@live.com>
2025-08-08 13:01:58 +1200

    second round of styling

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -4,8 +4,12 @@ import { Display } from './Display'
 function App() {
   return (
     <div>
-      <h1>Weather or Not (to go for a walk)</h1>
-      <p>Select Location</p>
+      <img className="weathericon" src="/images/weathericon.png" alt="" />
+      <h1>Weather or Not</h1>
+      <h2>...to go for a walk</h2>
+      <br></br>
+      <br></br>
+      <p>Select your Location!</p>
       <Display />
     </div>
   )
